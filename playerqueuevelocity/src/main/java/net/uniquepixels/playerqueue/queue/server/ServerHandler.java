6ab4dc6bb7b371b4f3a9cc160537874db7756d53
Anthony T <ungegrieftfirma@gmail.com>
2023-08-22 19:41:13 +0200

    feature(status from server): added method to get status from cloud server

--- playerqueuevelocity/src/main/java/net/uniquepixels/playerqueue/queue/server/ServerHandler.java
+++ playerqueuevelocity/src/main/java/net/uniquepixels/playerqueue/queue/server/ServerHandler.java
@@ -1,8 +1,13 @@
 package net.uniquepixels.playerqueue.queue.server;
 
 import com.google.gson.Gson;
+import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.val;
+import net.uniquepixels.playerqueue.DatabaseHandler;
+import net.uniquepixels.playerqueue.queue.server.httpbody.GotServerStatus;
+import net.uniquepixels.playerqueue.queue.server.httpbody.RequestNewCloudServer;
+import net.uniquepixels.playerqueue.queue.server.httpbody.RequestServerStatus;
 
 import java.io.InputStreamReader;
 import java.net.URI;
@@ -12,21 +17,20 @@ import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
 import java.util.concurrent.CompletableFuture;
 
+@RequiredArgsConstructor
 public class ServerHandler {
 
-    private final HttpClient client;
-
-    public ServerHandler() {
-        this.client = HttpClient.newHttpClient();
-    }
+    private final HttpClient client = HttpClient.newHttpClient();
+    private final DatabaseHandler databaseHandler;
+    private final Gson gson = new Gson();
 
     @SneakyThrows
-    public CompletableFuture<ServerData> requestNewServer(String task) {
+    public CompletableFuture<ServerData> requestNewServer(RequestNewCloudServer body) {
 
         val builder = HttpRequest.newBuilder()
-                .POST(HttpRequest.BodyPublishers.ofString("{\"task\": \"" + task + "\"}", StandardCharsets.UTF_8))
-                .uri(new URI("http://127.0.0.1:778/server/request?task=" + task))
-                .header("Bearer", "defaulttoken");
+                .POST(HttpRequest.BodyPublishers.ofString(this.gson.toJson(body), StandardCharsets.UTF_8))
+                .uri(new URI("http://127.0.0.1:778/cloud/request-server"))
+                .header("Bearer", databaseHandler.getHttpToken());
 
         var future = new CompletableFuture<ServerData>();
 
@@ -43,6 +47,30 @@ public class ServerHandler {
 
         return future;
 
+    }
+
+    @SneakyThrows
+    public CompletableFuture<ServerStatus> getStatusFromServer(RequestServerStatus body) {
+        val builder = HttpRequest.newBuilder()
+                .POST(HttpRequest.BodyPublishers.ofString(this.gson.toJson(body), StandardCharsets.UTF_8))
+                .uri(new URI("http://127.0.0.1:778/cloud/request-server-status"))
+                .header("Bearer", databaseHandler.getHttpToken());
+
+        var future = new CompletableFuture<ServerStatus>();
+
+        this.client.sendAsync(builder.build(), HttpResponse.BodyHandlers.ofInputStream()).thenAccept(inputStreamHttpResponse -> {
+
+            if (inputStreamHttpResponse.statusCode() != 200) {
+                future.complete(null);
+                return;
+            }
+
+            val gotServerStatus = new Gson().fromJson(new InputStreamReader(inputStreamHttpResponse.body()), GotServerStatus.class);
+            future.complete(ServerStatus.valueOf(gotServerStatus.getStatus().toUpperCase()));
+
+        });
+
+        return future;
     }
 
 }
